654ec86f5a19be016a86ade80cbae115deb951ac
bs <bscd2014@163.com>
2018-01-10 21:55:21 +0800

    first commit

---
+++ src/main/java/bs/org/commentaryserver/CommentaryServerApplication.java
@@ -0,0 +1,14 @@
+package bs.org.commentaryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentaryServerApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CommentaryServerApplication.class, args);
+    }
+}
---
+++ src/test/java/bs/org/commentaryserver/CommentaryServerApplicationTests.java
@@ -0,0 +1,18 @@
+package bs.org.commentaryserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommentaryServerApplicationTests
+{
+
+    @Test
+    public void contextLoads()
+    {
+    }
+
+}
